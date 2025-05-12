c072adddec874a22d12fce1c3e2bff876472e99b
Jean Ramos <jean_henriquez@hotmail.com>
2025-05-11 19:07:24 -0600

    resort model

---
+++ models/resort.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+
+const Resort = sequelize.define('resort', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  zipCode: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  }); 
+
+module.exports = Resort;
