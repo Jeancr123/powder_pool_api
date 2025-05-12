1c31238c8a037285aa882a709c28bce84c10c83b
Jean Ramos <jean_henriquez@hotmail.com>
2025-05-11 19:24:33 -0600

    adding seeds for resorts and users

---
+++ dev-data/resorts.js
@@ -0,0 +1,82 @@
+const planSeeds = 
+[
+  {
+    "name": "Park City Mountain Resort",
+    "address": "1345 Lowell Ave",
+    "city": "Park City",
+    "state": "UT",
+    "zipCode": "84060"
+  },
+  {
+    "name": "Deer Valley Resort",
+    "address": "2250 Deer Valley Dr S",
+    "city": "Park City",
+    "state": "UT",
+    "zipCode": "84060"
+  },
+  {
+    "name": "Snowbird",
+    "address": "9385 S Snowbird Center Dr",
+    "city": "Snowbird",
+    "state": "UT",
+    "zipCode": "84092"
+  },
+  {
+    "name": "Alta Ski Area",
+    "address": "Highway 210",
+    "city": "Alta",
+    "state": "UT",
+    "zipCode": "84092"
+  },
+  {
+    "name": "Solitude Mountain Resort",
+    "address": "12000 Big Cottonwood Canyon Rd",
+    "city": "Solitude",
+    "state": "UT",
+    "zipCode": "84121"
+  },
+  {
+    "name": "Brighton Resort",
+    "address": "8302 S Brighton Loop Rd",
+    "city": "Brighton",
+    "state": "UT",
+    "zipCode": "84121"
+  },
+  {
+    "name": "Snowbasin Resort",
+    "address": "3925 E Snowbasin Rd",
+    "city": "Huntsville",
+    "state": "UT",
+    "zipCode": "84317"
+  },
+  {
+    "name": "Powder Mountain",
+    "address": "6965 E Powder Mountain Rd",
+    "city": "Eden",
+    "state": "UT",
+    "zipCode": "84310"
+  },
+  {
+    "name": "Nordic Valley Ski Resort",
+    "address": "3567 Nordic Valley Way",
+    "city": "Eden",
+    "state": "UT",
+    "zipCode": "84310"
+  },
+  {
+    "name": "Eagle Point Resort",
+    "address": "150 S West Village Circle",
+    "city": "Beaver",
+    "state": "UT",
+    "zipCode": "84713"
+  },
+  {
+    "name": "Brian Head Resort",
+    "address": "329 S Hwy 143",
+    "city": "Brian Head",
+    "state": "UT",
+    "zipCode": "84719"
+  }
+]
+
+module.exports = planSeeds;
---
+++ dev-data/users.js
@@ -0,0 +1,7 @@
+const userSeeds = [
+    {
+        "email": "john.smith@example.com",
+        "password": "password123"
+    }
+]
+module.exports = userSeeds;
