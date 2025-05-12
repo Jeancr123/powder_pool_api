fe18cb0a2772f3b50f372c922155091cb7e011d8
Jean Ramos <jean_henriquez@hotmail.com>
2025-05-12 01:45:55 -0600

    restructured model relationships

--- models/index.js
+++ models/index.js
@@ -1,23 +1,76 @@
-const sequelize = require("../database");
-const Resort = require("./resort");
-const Carpool = require("./carpool");
-const User = require("./user");
-const Vehicle = require("./vehicle");
-const Conversation = require("./conversation");
-const ConversationMessage = require("./conversation_message");
+const sequelize = require('../database');
 
-Vehicle.belongsTo(User);
-Carpool.hasMany(User, {as: 'users'});
-Carpool.belongsTo(Resort);
-ConversationMessage.belongsTo(Conversation);
-ConversationMessage.belongsTo(User);
-User.hasMany(Carpool, {as: 'carpools'});
+const Resort = require('./resort');
+const Carpool = require('./carpool');
+const User = require('./user');
+const Vehicle = require('./vehicle');
+const Conversation = require('./conversation');
+const ConversationMessage = require('./conversation_message');
 
-  module.exports = {
-    Resort,
-    User,
-    Carpool,
-    Vehicle,
-    Conversation,
-    ConversationMessage,
-  }
+// ------------------------------
+// One-to-Many Associations
+// ------------------------------
+
+// Vehicles belong to a User
+Vehicle.belongsTo(User, { as: 'user', foreignKey: 'userId' });
+
+// A User can create many Carpools
+User.hasMany(Carpool, { as: 'createdCarpools', foreignKey: 'createdBy' });
+
+// A Carpool is created by a User
+Carpool.belongsTo(User, { as: 'creator', foreignKey: 'createdBy' });
+
+// A Carpool belongs to a Resort
+Carpool.belongsTo(Resort, { as: 'resort', foreignKey: 'resortId' });
+
+// Conversation belongs to a Carpool
+Conversation.belongsTo(Carpool, { foreignKey: 'carpoolId' });
+
+// A Conversation has many Messages
+Conversation.hasMany(ConversationMessage, {
+  as: 'messages',
+  foreignKey: 'conversationId'
+});
+
+// Each Message belongs to a Conversation
+ConversationMessage.belongsTo(Conversation, {
+  foreignKey: 'conversationId'
+});
+
+// Each Message is authored by a User
+ConversationMessage.belongsTo(User, {
+  as: 'user',
+  foreignKey: 'userId'
+});
+
+// ------------------------------
+// Many-to-Many Associations
+// ------------------------------
+
+// Users can join many Carpools
+User.belongsToMany(Carpool, {
+  through: 'CarpoolUser',
+  as: 'joinedCarpools',
+  foreignKey: 'userId'
+});
+
+// Carpools can have many Riders (Users)
+Carpool.belongsToMany(User, {
+  through: 'CarpoolUser',
+  as: 'participants',
+  foreignKey: 'carpoolId'
+});
+
+// ------------------------------
+// Export Models
+// ------------------------------
+
+module.exports = {
+  sequelize,
+  Resort,
+  User,
+  Carpool,
+  Vehicle,
+  Conversation,
+  ConversationMessage
+};
