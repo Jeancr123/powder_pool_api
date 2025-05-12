b060f27af9c5e53c22f23f76591f516245927ee9
Jean Ramos <jean_henriquez@hotmail.com>
2025-05-11 19:33:05 -0600

    getting carpools

---
+++ controllers/get_carpools.js
@@ -0,0 +1,12 @@
+const {Carpool}  = require('../models/index');
+
+
+module.exports = async (req, res) => {
+    try {
+      const allCarpools = await Carpool.findAll();
+      res.status(200).json(allPlans);
+    } catch (error) {
+      console.error('Error fetching carpools:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
