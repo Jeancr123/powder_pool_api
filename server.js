277f4699ac9a5c7067c1f6bf0efbdd1f2cf9dc38
Jean Ramos <jean_henriquez@hotmail.com>
2025-05-12 00:02:55 -0600

    temp testing with AWS

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors');
 const authenticateUser = require('./utils/auth_check')
-const signUp = require('./controllers/signup')
+const signUp = require('./controllers/sign_up')
 const login = require('./controllers/login')
-const getCarpools = require('./controllers/get_carpools')
-const newCarpool = require('./controllers/new_carpool')
-const joinCarpool = require('./controllers/join_carpool')
-const getCarpool = require('./controllers/get_carpool')
-const carpoolMessage = require('./controllers/carpool_message')
+const getResorts = require('./controllers/get_resorts')
+// const getCarpools = require('./controllers/get_carpools')
+// const newCarpool = require('./controllers/save_carpool')
+// const joinCarpool = require('./controllers/join_carpool')
+// const getCarpool = require('./controllers/get_carpool')
+// const carpoolMessage = require('./controllers/carpool_message')
 const seedAndSync = require('./controllers/seed_and_sync')
 
 
@@ -28,11 +29,12 @@ app.get('/', (req, res) => {
 app.get('/seed_and_sync', seedAndSync)
 app.post('/signup', signUp)
 app.post('/login', login)
-app.get('/carpools', getCarpools);
-app.post('/new_carpool', authenticateUser, newCarpool);
-app.get('/join_carpool', authenticateUser, joinCarpool);
-app.get('/get_carpool/:carpoolId', authenticateUser, getCarpool);
-app.post('/carpool_message', authenticateUser, carpoolMessage)
+// app.get('/carpools', getCarpools);
+app.get('/resorts', authenticateUser, getResorts);
+// app.post('/new_carpool', authenticateUser, newCarpool);
+// app.get('/join_carpool', authenticateUser, joinCarpool);
+// app.get('/get_carpool/:carpoolId', authenticateUser, getCarpool);
+// app.post('/carpool_message', authenticateUser, carpoolMessage)
 
 
 app.listen(port, () => {
