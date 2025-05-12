ad68d45f97843877db9d843fec36ff812b73ea52
Jean Ramos <jean_henriquez@hotmail.com>
2025-05-11 19:53:01 -0600

    using an object oriented approach in functions

---
+++ controllers/sign_up.js
@@ -0,0 +1,31 @@
+const {User} = require('../models/index');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+
+module.exports = async (req, res) => {
+    const userJson = req.body;
+  
+    try {
+      const existingUser = await User.findOne({ where: { email: userJson.email } });
+      if (existingUser) {
+        return res.status(409).json({ error: 'email_already_in_use' });
+      }
+
+       userJson.password = await bcrypt.hash(userJson.password, 10);
+
+      const newUser = await User.create(userJson);
+
+      const token = jwt.sign(
+        { userId: newUser.id, userEmail: newUser.email },
+        process.env.JWT_KEY,
+        { expiresIn: '1h' }
+      );
+
+      return res.status(201).json({ token });
+    } catch (error) {
+        console.error('Error during signup:', error);
+        res.status(500).json({ error: 'internal_server_error' });
+    }
+};
