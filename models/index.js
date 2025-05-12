f7320ccb4c292f9cad23b82b96102d93842b3ef8
Jean Ramos <jean_henriquez@hotmail.com>
2025-05-11 19:32:07 -0600

    adding data models

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const sequelize = require("../database");
+const Resort = require("./resort");
+const Carpool = require("./carpool");
+const User = require("./user");
+const Vehicle = require("./vehicle");
+const Conversation = require("./conversation");
+const ConversationMessage = require("./conversation_message");
+
+Vehicle.belongsTo(User);
+Carpool.hasMany(User, {as: 'users'});
+Carpool.belongsTo(Resort);
+CarpoolMessage.belongsTo(Conversation);
+CarpoolMessage.belongsTo(User);
+User.hasMany(Carpool, {as: 'carpools'});
+
+  module.exports = {
+    Resort,
+    User,
+    Carpool,
+    Vehicle,
+    Conversation,
+    ConversationMessage,
+  }
